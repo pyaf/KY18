bad82312f09a85790c0c41d0dadaf210fc5dc408
mukesh <mdeogune@gmail.com>
2017-08-28 23:27:51 +0530

    ca-backend new

--- kashiyatra/urls.py
+++ kashiyatra/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.views.defaults import page_not_found
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^account/social/signup/',TemplateView.as_view(template_name='ca-redirect.html')),
     url(r'^account/email$',page_not_found, {'exception': Exception('Not Found')}),
     url(r'^account/', include('allauth.urls')),
     url(r'^', include('users.urls')),
