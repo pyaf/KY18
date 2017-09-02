905bf81441d1d2b3a42ba878c2d12ad212a342ba
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-09-02 23:29:55 +0530

    Posts in admin

--- etc/admin.py
+++ etc/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Notifications
+from .models import Notifications, Post
 # Register your models here.
 admin.site.register(Notifications)
+admin.site.register(Post)
