42ba478be41b22e9b13bb966de1e31eabd276c82
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-09-24 18:43:14 +0530

    bugfix

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     url(r'^$', IndexView.as_view()),
     url(r'^ca-form/$', CaFormView),
-    url(r'^dashboard$', DashboardView, name="ca-dashboard"),
+    url(r'^dashboard', DashboardView, name="ca-dashboard"),
     url(r'^ca-guidlines/$', GuidlinesView, name="ca-guidlines"),
     url(r'^ca-profile/', CAProfileUpdateView, name="ca-profile"),
     url(r'^leaderboard/$', LeaderBoardView, name="ca-leaderboard"),
