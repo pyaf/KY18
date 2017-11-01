ca47ea0744f19761b02b59d3e114af856142a74e
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-11-01 11:41:16 +0530

    update points script updated

--- scripts/updatepoints.py
+++ scripts/updatepoints.py
@@ -8,31 +8,18 @@ django.setup()
 from etc.models import *
 from users.models import *
 
-print('updating pr points \n\n')
+print('updating points \n\n')
+
 for ca in CAProfile.objects.all():
+	ca_id = ca.ca_id
 	try:
 		total_prs = PublicRelation.objects.filter(ca=ca).count()	
+		regs = KYProfile.objects.filter(referralCode=ca_id).count()
 		point = Point.objects.get(ca=ca)
 		point.pr_points = 10 * total_prs
+		point.reg_points = regs * 30
 		point.save()
 		print(ca, 'done')
 	except Exception as e:
 		print(e)
 
-print('reg points to be set zero\n\n')
-for point in Point.objects.all():
-	point.reg_points = 0
-	point.save()
-
-print('\nupdating reg points\n\n')
-for kyprofile in KYProfile.objects.all():
-	ca_id = kyprofile.referralCode
-	print(ca_id)
-	try:
-		ca = CAProfile.objects.get(ca_id=ca_id)
-		print(ca)
-		point = Point.objects.get(ca=ca)
-		point.reg_points += 30	
-		point.save()
-	except Exception as e:
-		pass
