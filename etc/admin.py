4cdf1d52e4fc9a1ee315048de056d4f500c1bf0a
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-11-01 11:57:45 +0530

    points admin search bugfix

--- etc/admin.py
+++ etc/admin.py
@@ -13,7 +13,7 @@ class PostAdmin(admin.ModelAdmin):
 class PointAdmin(admin.ModelAdmin):
 
     list_display = ('ca', 'reg_points', 'fb_points', 'pr_points', 'extra_points', 'total_points')
-    search_fields = ('ca',)
+    search_fields = ('ca__kyprofile__full_name',)
 
 admin.site.register(Point, PointAdmin)
 
