7822374b6b2851d55a46aa0aa3e209c509ef9471
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-12-23 12:24:43 +0530

    admin tweaks

--- event/admin.py
+++ event/admin.py
@@ -5,6 +5,16 @@ from event.models import *
 from django.contrib import admin
 admin.site.register(Event)
 admin.site.register(ParentEvent)
-admin.site.register(Team)
+
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+
+    def college(obj):
+        return obj.teamLeader.college.collegeName
+
+    list_display = ('event', 'teamName', 'teamId', 'teamLeader', college)
+    search_fields = ('event__eventName', 'teamName', 'teamId', 'teamLeader__ky_id', 
+    					'teamLeader__college__collegeName')
+
+admin.site.register(Team, TeamAdmin)
